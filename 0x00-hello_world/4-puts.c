0c1b43270a8920563905893f5002e2b7d5921939
FSA-22 <simeonstat@gmail.com>
2022-10-18 02:41:22 -0700

    project 0x02 task zero

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdios.h>
+/*
+ * main - using PUTs function to prnt a line of code
+ *
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	reture (0);
+}
+
