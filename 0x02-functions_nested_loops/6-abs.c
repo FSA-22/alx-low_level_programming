16f5cc048a5b55cc7cad3a05284bcc8b3711b7ab
FSA-22 <simeonstat@gmail.com>
2022-10-18 07:12:22 -0700

    project 0x02 task six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -  function that computes the absolute value of an integer
+ * @n: interger.
+ * Return: value of integer (int).
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
