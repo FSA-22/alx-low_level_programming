18dc8c9088dc0d0879f3555f41055e0a5062d4f3
FSA-22 <simeonstat@gmail.com>
2022-10-19 02:32:52 -0700

    project 0x02 task seven

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit of a number.
+ *
+ * Return: function will return 0.
+ *
+ * @n: int n i the function set of numbers.
+ *
+ * @x: int x assigned to last number.
+ */
+int print_last_digit(int)
+{
+	int n;
+	int x;
+
+	n = rand();
+	x = n = % 10;
+	printf("%d" n, x);
+	return (0);
+}
