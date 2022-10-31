3ff9b34d95f01d867dc4bfbcbbb6935b05614efc
FSA-22 <simeonstat@gmail.com>
2022-10-31 13:53:39 -0700

    0x07, task 1 to 9

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  *@b: constant byte that will be written into the momory
  *@n: amount of times the byte will be written into the memory
  *
- *Return: address of the memory where thebyte was written
+ *Return: address of the memory where the byte was written
  */
 char *_memset(char *s, char b, unsigned int n)
 {
